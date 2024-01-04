577113845302a1f2da1fd237b9a7c97857ed9730
sayaka-0219 <chos.38@icloud.com>
2024-01-04 14:33:54 +0900

    hello worldの実装にPATCHメソッドを追加、Content-Typeがapplication/jsonになっているHiroshiクラスを追加

--- src/main/java/com/assignment6/hello/HelloController.java
+++ src/main/java/com/assignment6/hello/HelloController.java
@@ -1,13 +1,25 @@
 package com.assignment6.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-
-        return "hello world";
+       return "hello world";
     }
+
+    @PatchMapping("/hello")
+    public String patchHello(){
+        return "HELLO WORLD!!!";
+    }
+
+    @GetMapping("/hiroshi")
+    public Hiroshi getHiroshi() {
+        return new Hiroshi("野原ひろし", 35, 180);
+    }
+
+
 }
---
+++ src/main/java/com/assignment6/hello/Hiroshi.java
@@ -0,0 +1,28 @@
+package com.assignment6.hello;
+
+
+
+public class Hiroshi {
+    private String name;
+    private  int age;
+    private  int height;
+
+    public  Hiroshi(String name, int age, int height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
